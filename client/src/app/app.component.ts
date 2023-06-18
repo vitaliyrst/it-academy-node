17ab8d2f956483f3cc3662cf5b6da2aad60058af
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2023-06-18 21:24:33 +0200

    Add client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {RestService} from "./core/services/rest.service";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+    title = 'client';
+
+    constructor(
+        private rest: RestService
+    ) {
+    }
+
+    ngOnInit() {
+        this.rest.get('votes/variants')
+            .subscribe({
+                next: response => {
+                    console.log(response)
+                }
+            })
+    }
+}
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, {Express} from 'express';
+import cors from 'cors';
 import mainRouter from "./routes/main.router";
 import dotenv from 'dotenv';
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app: Express = express();
 const port: string | number = process.env.PORT || 5000;
 
+app.use(cors());
 app.use('/rest', mainRouter);
 
 app.listen(port, () => {
