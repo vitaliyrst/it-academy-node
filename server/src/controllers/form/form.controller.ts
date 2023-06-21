4f7f5eabd835a4525f3dcd1f4d7d8922e1d8c385
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2023-06-21 10:39:39 +0200

    Added second job

--- server/src/controllers/form/form.controller.ts
+++ server/src/controllers/form/form.controller.ts
@@ -5,7 +5,7 @@ class FormController {
     newForm!: string;
     form = async (req: Request, res: Response) => {
         try {
-            const form = this.newForm || await FormService.getHtmlForm();
+            const form = await FormService.getHtmlForm();
             return res.status(200).send(form);
         } catch (err) {
             return res.status(500).json({
@@ -18,7 +18,7 @@ class FormController {
     submit = async (req: Request, res: Response) => {
         try {
             this.newForm = await FormService.validateForm(req.body);
-            return res.redirect('/rest/form');
+            return res.status(200).send(this.newForm);
         } catch (err) {
             return res.status(500).json({
                 success: false,
